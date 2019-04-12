2ca913fa34c7bae4d113f390cdabd4ad1c671bed
sandeepkumy <sandeepkumy@L-6885.kpit.com>
2019-04-12 18:35:19 +0530

    Interceptor

---
+++ User/src/main/java/com/user/main/interceptor/UserServiceInterceptor.java
@@ -0,0 +1,37 @@
+package com.user.main.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class UserServiceInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws Exception
+	{
+		String req=request.getRequestURI().toString();
+		
+		System.out.println("PreHandler Executed.."+req);
+		return true;
+		
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request,HttpServletResponse response, Object handler,ModelAndView modelAndView) throws Exception
+	{
+		System.out.println("PostHandler Executed..");
+		
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,HttpServletResponse response, Object handler,Exception exception) throws Exception
+	{
+		System.out.println("AfterCompletion Executed..");
+		
+	}
+
+}
---
+++ User/src/main/java/com/user/main/interceptor/UserServiceInterceptorAppConfig.java
@@ -0,0 +1,21 @@
+package com.user.main.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class UserServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+	
+	@Autowired
+	UserServiceInterceptor userServiceInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(userServiceInterceptor);
+	}
+
+}
